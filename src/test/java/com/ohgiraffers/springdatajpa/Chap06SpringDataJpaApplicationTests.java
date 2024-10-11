afc3cc554993ce2f8e5fde0ad961aea3416937aa
Jinny04 <cje364182@gmail.com>
2024-10-11 17:47:06 +0900

    feat: Test를 위한 main 세팅

--- src/test/java/com/ohgiraffers/springdatajpa/Chap06SpringDataJpaApplicationTests.java
+++ src/test/java/com/ohgiraffers/springdatajpa/Chap06SpringDataJpaApplicationTests.java
@@ -1,13 +1,47 @@
 package com.ohgiraffers.springdatajpa;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.*;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 class Chap06SpringDataJpaApplicationTests {
 
-    @Test
-    void contextLoads() {
+    private static EntityManagerFactory entityManagerFactory;
+    private EntityManager entityManager;
+
+    @BeforeAll
+    public static void initFactory() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("jpatest");
     }
 
+    @BeforeEach
+    public void initManager() {
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
+    @AfterAll
+    public static void closeFactory() {
+        entityManagerFactory.close();
+    }
+
+    @AfterEach
+    public void closeManager() {
+        entityManager.close();
+    }
+
+    // selectAll Test
+
+    // selectByCode Test
+
+    // querySelect Test
+
+    // insert Test
+
+    // update Test
+
+    // delete Test
 }
