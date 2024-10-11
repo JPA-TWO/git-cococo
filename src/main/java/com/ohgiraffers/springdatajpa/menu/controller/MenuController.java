706716b7e0580d561926e083510f186a9684161b
Wondear <vytpals00@gmail.com>
2024-10-11 13:16:37 +0900

    [feat] #3 메뉴 가격 쿼리문을 통한 SELECT 완료

--- src/main/java/com/ohgiraffers/springdatajpa/menu/controller/MenuController.java
+++ src/main/java/com/ohgiraffers/springdatajpa/menu/controller/MenuController.java
@@ -1,8 +1,16 @@
 package com.ohgiraffers.springdatajpa.menu.controller;
 
+import com.ohgiraffers.springdatajpa.menu.dto.MenuDTO;
+import com.ohgiraffers.springdatajpa.menu.service.MenuService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 /* 설명. @Slf4j(Simple Logging Facade for Java):
  *  Lombok 라이브러리의 어노테이션으로 클래스에 자동으로 SLF4J Logger 인스턴스를 추가해준다.
@@ -14,4 +22,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/menu")
 public class MenuController {
 
+    private MenuService menuService;
+
+//    @Autowired
+    public MenuController(MenuService menuService) {
+        this.menuService = menuService;
+    }
+
+    //리다이렉트
+    @GetMapping("/querymethod")
+    public void queryMethodPage() {}
+
+    //가격으로 서치 (일정 가격 "초과" 목록 조회) => 다중 조회
+    @GetMapping("/search")
+    public String finfByMenu(@RequestParam Integer menuPrice, Model m){
+        //form으로 온 데이터 받기
+        //받은 데이터로 처리한 뒤 모델에 적용
+        List<MenuDTO> menus = menuService.findByMenuPrice(menuPrice);
+        m.addAttribute("menuPrice", menuPrice);
+        m.addAttribute("menuList", menus);
+
+        // 리다이렉트
+
+        return "menu/searchResult";
+    }
 }
--- src/main/java/com/ohgiraffers/springdatajpa/menu/service/MenuService.java
+++ src/main/java/com/ohgiraffers/springdatajpa/menu/service/MenuService.java
@@ -1,7 +1,27 @@
 package com.ohgiraffers.springdatajpa.menu.service;
 
+import com.ohgiraffers.springdatajpa.menu.dto.MenuDTO;
+import com.ohgiraffers.springdatajpa.menu.entity.Menu;
+import com.ohgiraffers.springdatajpa.menu.repository.MenuRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MenuService {
+    private final ModelMapper modelMapper;
+    MenuRepository menuRepository;
+
+//    @Autowired
+    public MenuService(MenuRepository menuRepository, ModelMapper modelMapper) {
+        this.menuRepository = menuRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    public List<MenuDTO> findByMenuPrice(Integer menuPrice) {
+        List<Menu> menus = menuRepository.findByMenuPriceGreaterThan(menuPrice);
+
+        return  menus.stream().map(menu -> modelMapper.map(menu,MenuDTO.class)).toList();
+    }
 }
